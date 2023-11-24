6f2731a6d6ac1db45c2b9e0cd87fd7827217677c
YoasterToaster <joefoschia@gmail.com>
2023-11-23 21:20:54 -0500

    .

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ fetchMyIP((error, ip) => {
     fetchISSFlyOverTimes(null, data);
   });
 
-  nextISSTimesForMyLocation((error, passTimes) => {
-    if (error) {
-      return console.log("It didn't work!", error);
-    }
-    // success, print out the deets!
-    console.log(passTimes);
-  });
+  // nextISSTimesForMyLocation((error, passTimes) => {
+  //   if (error) {
+  //     return console.log("It didn't work!", error);
+  //   }
+  //   // success, print out the deets!
+  //   console.log(passTimes);
+  // });
 
   console.log('It worked! Returned IP:' , ip);
   
